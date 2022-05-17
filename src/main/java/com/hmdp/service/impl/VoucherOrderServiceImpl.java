4555eacc627a11c4c58d1111278c47f8cfbafdef
biubiubiuer <784470655@qq.com>
2022-05-17 21:11:02 +0800

    悲观锁实现一人一单

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,5 +1,11 @@
 package com.hmdp.service.impl;
 
+import java.time.LocalDateTime;
+
+import javax.annotation.Resource;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.SeckillVoucher;
@@ -9,10 +15,6 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
-import java.time.LocalDateTime;
 
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
     
@@ -23,7 +25,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
     
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         
         // 1. 查询优惠券
@@ -46,34 +47,61 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 库存不足
             return Result.fail("库存不足!");
         }
-        
-        // 5. 扣减库存
-        boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
-                .gt("stock", 0)
-                .update();
-        
-        if (!success) {
-            // 扣减失败
-            return Result.fail("库存不足!");
-        }
-        
-        // 6. 创建订单
-        VoucherOrder voucherOrder = new VoucherOrder();
-        // 6.1 订单 id
-        long orderId = redisIdWorker.nextId("order");
-        voucherOrder.setId(orderId);
-        // 6.2 用户 id
-        Long userId = UserHolder.getUser().getId();
-        voucherOrder.setUserId(userId);
-        // 6.3 代金券 id
-        voucherOrder.setVoucherId(voucherId);
-        
-        save(voucherOrder);
 
-        // 7. 返回订单 id
+
+        return createVoucherOrder(voucherId);
+    }
+
+    /**
+     * @author wendong
+     * @date 2022/5/17
+     * @methodName createVoucherOrder
+     * description 以用户id加悲观锁, 实现一人一单
+     * @param voucherId
+     * @return com.hmdp.dto.Result 
+     *
+     **/
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 5. 一人一单
+        Long userId = UserHolder.getUser().getId();
         
-        return Result.ok(voucherId);
+        synchronized (userId.toString().intern()) {
+            // 5.1 查询订单
+            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+
+            // 5.2 判断是否存在
+            if (count > 0) {
+                // 用户已经购买过了
+                return Result.fail("用户已经购买过一次!");
+            }
+
+            // 6. 扣减库存
+            boolean success = seckillVoucherService.update()
+                    .setSql("stock = stock - 1")
+                    .eq("voucher_id", voucherId)
+                    .gt("stock", 0)
+                    .update();
+
+            if (!success) {
+                // 扣减失败
+                return Result.fail("库存不足!");
+            }
+            // 7. 创建订单
+            VoucherOrder voucherOrder = new VoucherOrder();
+            // 7.1 订单 id
+            long orderId = redisIdWorker.nextId("order");
+            voucherOrder.setId(orderId);
+            // 7.2 用户 id
+            voucherOrder.setUserId(userId);
+            // 7.3 代金券 id
+            voucherOrder.setVoucherId(voucherId);
+
+            save(voucherOrder);
+
+            // 8. 返回订单 id
+
+            return Result.ok(voucherId);
+        }
     }
 }
