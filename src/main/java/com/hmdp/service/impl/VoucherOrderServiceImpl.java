8b10fd7e99f917a9ebff87a5cea609a8f07479a9
biubiubiuer <784470655@qq.com>
2022-05-19 03:44:54 +0800

    秒杀优化 基于阻塞队列实现秒杀异步下单

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,9 +1,15 @@
 package com.hmdp.service.impl;
 
 import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,6 +25,9 @@ import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder>
     implements IVoucherOrderService {
@@ -41,6 +50,78 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         SECKILL_SCRIPT.setLocation(new ClassPathResource("seckill.lua"));
         SECKILL_SCRIPT.setResultType(Long.class);
     }
+    
+    private BlockingQueue<VoucherOrder> orderTasks = new ArrayBlockingQueue<>(1 << 20);
+    private static final ExecutorService SECKILL_ORDER_EXECUTOR = Executors.newSingleThreadExecutor();
+    
+    @PostConstruct
+    private void init() {
+        SECKILL_ORDER_EXECUTOR.submit(new VoucherOrderHandler());
+    }
+    
+    private class VoucherOrderHandler implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                // 1. 获取队列中的订单信息
+                try {
+                    VoucherOrder voucherOrder = orderTasks.take();
+                    // 2. 创建订单
+                    createVoucherOrder(voucherOrder);
+                } catch (Exception e) {
+                    log.error("处理订单移除", e);
+                }
+            }
+        }
+    }
+
+    private void createVoucherOrder(VoucherOrder voucherOrder) {
+        Long userId = voucherOrder.getUserId();
+        Long voucherId = voucherOrder.getVoucherId();
+
+        // 创建锁对象
+        RLock redisLock = redissonClient.getLock("lock:order:" + userId);
+
+        // 尝试获取锁
+        boolean isLock = redisLock.tryLock();
+
+        // 判断
+        if (!isLock) {
+            // 获取锁失败, 直接返回失败 或者 重试
+            log.error("不允许重复下单!");
+            return;
+        }
+
+        try {
+            // 5.1 查询订单
+            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+
+            // 5.2 判断是否存在
+            if (count > 0) {
+                // 用户已经购买过了
+                log.error("不允许重复下单!");
+                return;
+            }
+
+            // 6. 扣减库存
+            boolean success = seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId)
+                .gt("stock", 0).update();
+
+            if (!success) {
+                // 扣减失败
+                log.error("库存不足!");
+                return;
+            }
+            
+            save(voucherOrder);
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 释放锁
+            redisLock.unlock();
+        }
+    }
 
     @Override
     public Result seckillVoucher(Long voucherId) {
@@ -59,9 +140,17 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail(r == 1 ? "库存不足!" : "不能重复下单!");
         }
         // 2.2 为0, 有购买资格, 把下单信息保存到阻塞队列
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 2.3 订单 id
         long orderId = redisIdWorker.nextId("order");
-
-        // TODO 保存阻塞队列
+        voucherOrder.setId(orderId);
+        // 2.4 用户 id
+        voucherOrder.setUserId(userId);
+        // 2.5 代金券 id
+        voucherOrder.setVoucherId(voucherId);
+        
+        // 2.6 放入阻塞队列
+        orderTasks.add(voucherOrder);
         
         // 3. 返回订单 id
         return Result.ok(orderId);
